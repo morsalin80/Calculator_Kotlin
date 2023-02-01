bb43bf5785e751b02ee5c13b9905ea70041c7e92
Md Morsalin Hossain <mdmorsalin80@gmail.com>
2023-02-01 22:24:12 +0600

    initial commit

---
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.mycalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private var tvInput: TextView?= null
+    private var lastNumeric: Boolean = false
+    private var lastDot:Boolean = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tvInput = findViewById(R.id.tvInput)
+    }
+    fun onDigit(view: View) {
+        tvInput?.append((view as Button).text)
+        lastNumeric = true
+    }
+    fun onClear(view: View) {
+        tvInput?.text = ""
+        lastDot = false
+    }
+    fun onDecimal(view:View){
+        if(lastNumeric && !lastDot) {
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+    fun onOperator(view: View) {
+        tvInput?.text?.let {
+            if(lastNumeric && !isOperatorAdded(it.toString())) {
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+
+    }
+
+    fun onEqual(view: View) {
+        if(lastNumeric) {
+            var tvValue = tvInput?.text.toString()
+            var prefix =""
+            try {
+                if(tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+                val splitValue = tvValue.split("-")
+                var one = splitValue[0]
+                var two = splitValue[1]
+                if(prefix.isNotEmpty()) {
+                    one = prefix + one
+                }
+                tvInput?.text = (one.toDouble() - two.toDouble()).toString()
+            }catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun isOperatorAdded(value: String): Boolean {
+        return if(value.startsWith("-")) {
+            false
+        }else {
+            value.contains("/")
+                    || value.contains("*")
+                    || value.contains("+")
+                    ||value.contains("-")
+        }
+    }
+}
